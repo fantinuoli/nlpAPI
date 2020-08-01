0d8adc3b08ca7b39f178dca47327ba0977a33aa9
FC <61109621+fantinuoli@users.noreply.github.com>
2020-08-01 15:55:15 +0200

    Prepared first release

--- app/nlpAPI.py
+++ app/nlpAPI.py
@@ -14,51 +14,42 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+#this is the web interface to demonstrate the NER extraction
 @app.route('/process',methods=["POST"])
 def process():
 	if request.method == 'POST':
-		#choice = request.form['taskoption']
 		rawtext = request.form['rawtext']
 		doc = nlp(rawtext)
 		mkd_text = displacy.render(doc,style='ent')
-		value = Markup(mkd_text)
+		mkd_text_coded = Markup(mkd_text)
 
-		d = []
-		for ent in doc.ents:
-			d.append((ent.label_, ent.text))
-			df = pd.DataFrame(d, columns=('named entity', 'output'))
-			ORG_named_entity = df.loc[df['named entity'] == 'ORG']['output']
-			PERSON_named_entity = df.loc[df['named entity'] == 'PERSON']['output']
-			GPE_named_entity = df.loc[df['named entity'] == 'GPE']['output']
-			MONEY_named_entity = df.loc[df['named entity'] == 'MONEY']['output']
-			
-			results_ORG = ORG_named_entity
-			num_of_results_ORG = len(results_ORG)
-			results_PERSON = PERSON_named_entity
-			num_of_results_PERSON = len(results_PERSON)
-			results_GPE = GPE_named_entity
-			num_of_results_GPE = len(results_GPE)
-			results_MONEY = MONEY_named_entity
-			num_of_results_MONEY = len(results_MONEY)
-            
-			num_of_results = num_of_results_ORG + num_of_results_PERSON + num_of_results_GPE + num_of_results_MONEY
-	
-	return render_template("index.html",mkd_text=value, results_ORG=results_ORG,results_PERSON=results_PERSON,results_GPE=results_GPE, results_MONEY=results_MONEY, num_of_results = num_of_results)
+	return render_template("index.html", mkd_text=mkd_text_coded)
 
+#this needs to be deleted, it has no function 
 @app.route('/ner')
 def api_articles():
     return 'List of ' + url_for('api_articles')
 
+#this the API call
+#I need to modify it in order to extract ALL or a subset of NER
 @app.route('/ner/<rawtext>')
 def getNER(rawtext):
     doc = nlp(rawtext)
     d = []
+    num_of_results_GPE = 0
     for ent in doc.ents:
         d.append((ent.label_, ent.text))
         df = pd.DataFrame(d, columns=('named entity', 'output'))
         GPE_named_entity = df.loc[df['named entity'] == 'GPE']['output']
-
-    return GPE_named_entity.to_json(orient='split')
+        
+        #to get number of NER
+        results_GPE = GPE_named_entity
+        num_of_results_GPE = len(results_GPE)
+    
+    if num_of_results_GPE > 0:
+        return GPE_named_entity.to_json(orient='split')
+    else:
+        return "undef"#need to return a useful answer
 
 if __name__ == '__main__':
     app.run(debug=True)
